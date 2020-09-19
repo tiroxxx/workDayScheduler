07de7e247dfb4de1a352311713f1c3980ce9edb2
Christian Marquez <christianmarquez41@gmail.com>
2020-09-18 21:25:52 -0400

    added functionality to the buttons and textareas

--- script.js
+++ script.js
@@ -2,29 +2,99 @@ var currentDate = $("#currentDay");
 // display current date on the page
 currentDate.text(moment().format("dddd, MMMM Do"));
 
-var textBox1 = $("#9");
-var textBox2 = $("#10");
-var textBox3 = $("#11");
-var textBox4 = $("#12");
-var textBox5 = $("#13");
-var textBox6 = $("#14");
-var textBox7 = $("#15");
-var textBox8 = $("#16");
-var textBox9 = $("#17");
+//adding the inputs in the locar storage
+$("#btn9").on("click", function () {
+    localStorage.setItem("hour-9", $("#9").val())
+});
+
+$("#btn10").on("click", function () {
+    localStorage.setItem("hour-10", $("#10").val())
+});
+
+$("#btn11").on("click", function () {
+    localStorage.setItem("hour-11", $("#11").val())
+});
+
+$("#btn12").on("click", function () {
+    localStorage.setItem("hour-12", $("#12").val())
+});
+
+$("#btn13").on("click", function () {
+    localStorage.setItem("hour-13", $("#13").val())
+});
+
+$("#btn14").on("click", function () {
+    localStorage.setItem("hour-14", $("#14").val())
+});
+
+$("#btn15").on("click", function () {
+    localStorage.setItem("hour-15", $("#15").val())
+});
+
+$("#btn16").on("click", function () {
+    localStorage.setItem("hour-16", $("#16").val())
+});
+
+$("#btn17").on("click", function () {
+    localStorage.setItem("hour-17", $("#17").val())
+});
 
 
-
-
-
-console.log(moment().format("H"));
-
-
-
-
-
-function setColor () {
-    console.log($(this).attr("data-hour"));
-    if($(this).attr("data-hour") < moment().format("H")) {
+init();
+// initialize page
+function init() {
+    var info = localStorage.getItem("hour-9");
+    if(info !== null){
+       $("#9").text(info);
     }
 
+    info = localStorage.getItem("hour-10");
+    if(info !== null){
+       $("#10").text(info);
+    }
+
+    info = localStorage.getItem("hour-11");
+    if(info !== null){
+       $("#11").text(localStorage.getItem("hour-11")) 
+    }
+
+    info = localStorage.getItem("hour-12");
+    if(info !== null){
+       $("#12").text(localStorage.getItem("hour-12")) 
+    }
+
+    var info = localStorage.getItem("hour-13");
+    if(info !== null){
+       $("#13").text(localStorage.getItem("hour-13")) 
+    }
+
+    var info = localStorage.getItem("hour-14");
+    if(info !== null){
+       $("#14").text(localStorage.getItem("hour-14")) 
+    }
+
+    var info = localStorage.getItem("hour-15");
+    if(info !== null){
+       $("#15").text(localStorage.getItem("hour-15")) 
+    }
+
+    var info = localStorage.getItem("hour-16");
+    if(info !== null){
+       $("#16").text(localStorage.getItem("hour-16")) 
+    }
+
+    var info = localStorage.getItem("hour-17");
+    if(info !== null){
+       $("#17").text(localStorage.getItem("hour-17")) 
+    }
+    
+    var currentHour = moment().format("H");
+    
+    console.log(currentHour);
+
+    if(currentHour == $(".time-block")) 
 }
+
+
+
+
